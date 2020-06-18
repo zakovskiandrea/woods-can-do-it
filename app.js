a45d7b07d66f624a7ac8f5d65661c0bb93d0e362
Andrea Zakovski <andreazakovski@Andreas-MacBook-Pro.local>
2020-06-18 12:05:32 +0200

    about section added

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".slider-about").slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    variableWidth: true,
+    prevArrow: ".slider-btn--prev",
+    nextArrow: ".slider-btn--next",
+  });
+});
